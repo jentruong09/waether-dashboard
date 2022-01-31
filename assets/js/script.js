dd12dafbc6deed228e098159f25f63de0b9c6c78
Jen Truong <jennifertruong09@gmail.com>
2022-01-30 21:07:11 -0500

    created the layout for the web app, added pictures, some cards, and styling

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let weatherSearch = {
+    "apiKey" : "174c27157f74315bc7f7708bb00e7190",
+    fetchWeather: function() {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=new%20york&appid=174c27157f74315bc7f7708bb00e7190").then
+    }
+}
