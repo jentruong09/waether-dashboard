afee7be36050875cd35b3077417d026b04ec98d6
Jen Truong <jennifertruong09@gmail.com>
2022-01-31 20:59:30 -0500

    added some code for the local storage of previous searches - unable to test due to being blocked for too many calls due to a bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,20 +16,47 @@
 
 // var
 
+//get new api key?
+var API_KEY = "ed5364c18831aa760172c5c93330a21e";
+//var cityInput = document.querySelector(".form-control")
+//var searchBtn = document.getElementById("#button-addon2")
 
-var API_KEY = "174c27157f74315bc7f7708bb00e7190";
-var city = document.querySelector(".form-control")
+searchCity = $(".form-control").val();
+//console.log(cityInput)
 
 var getWeather = function () {
-    var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
+    var apiURL = "http://api.openweathermap.org/data/2.5/weather?q="+ searchCity + "&units=imperial" +"&appid=" + API_KEY;
 
     fetch(apiURL)
         .then(function(response){
-            console.log(response)
-            //return response.json();
+            console.log(response);
+            return response.json();
         })
-        //.then(function(data)){
-           // console.log()
-       // }
+        .then(function(data){
+           console.log(data)
+        })
+        
+}; console.log(getWeather)
+
+
+
+
+$('button').on("click", function(event){
+    event.preventDefault();
+    searchCity = $(".form-control").val();
+    getWeather();
+    
+
+    var searchContent = $(this).sibilings("input").val();
+    var storeArray = [];
+    storeArray.push(searchContent)
+    local.storage("storedCityName", JSON.stringify(storeArray))
+
+    getStoredItem()
+});
+
+function getStoredItem() {
+    var pastSearch = JSON.parse(localStorage.getItem("storedCityName"))
+
 
 }
