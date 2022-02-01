fb940a3cde72d2af9e1bbcc79c5592f8c73ab34c
Jen Truong <jennifertruong09@gmail.com>
2022-01-31 19:36:53 -0500

    made the cards for the search area and today's weather the same size and on the same row plus started on the js- formating the url data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
-let weatherSearch = {
-    "apiKey" : "174c27157f74315bc7f7708bb00e7190",
-    fetchWeather: function() {
-        fetch("http://api.openweathermap.org/data/2.5/weather?q=new%20york&appid=174c27157f74315bc7f7708bb00e7190").then
-    }
+
+// var API_KEY = "174c27157f74315bc7f7708bb00e7190";
+
+
+
+
+
+// let weatherSearch = {
+//     var fetchWeather = function(){
+//         fetch("http://api.openweathermap.org/data/2.5/weather?q=new%20york&appid=174c27157f74315bc7f7708bb00e7190")
+//         .then((response) = response.json())
+//         .then((data) = console.log(data))
+//     }
+// }
+
+
+// var
+
+
+var API_KEY = "174c27157f74315bc7f7708bb00e7190";
+var city = document.querySelector(".form-control")
+
+var getWeather = function () {
+    var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
+
+    fetch(apiURL)
+        .then(function(response){
+            console.log(response)
+            //return response.json();
+        })
+        //.then(function(data)){
+           // console.log()
+       // }
+
 }
